5608da3dc85ef247677e55b746f8229cf825fc93
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2024-10-01 18:31:02 +0530

    Program to take input from user

---
+++ Inputfromuser.java
@@ -0,0 +1,11 @@
+// Program to take input from user
+import java.util.Scanner;
+public class Inputfromuser{
+    public static void main(String args[]){
+        //creating object of scanner class
+        Scanner obj = new  Scanner(System.in);
+        System.out.println("enter a number:=");
+        int num =obj.nextint();
+        System.out.println("entered number is:="+num);
+            }
+}
